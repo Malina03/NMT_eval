b84e845bf15d7a00f662b8064f45d83e9e84027a
Malina03 <malina.chichirau@gmail>
2023-06-28 20:31:49 +0200

    fixed path

--- src/train.sh
+++ src/train.sh
@@ -23,8 +23,8 @@ log_file="/scratch/hb-macocu/NMT_eval/en-sq/logs/train.log"
 
 python /home1/s3412768/NMT_eval/src/train.py \
     --root_dir $root_dir \
-    --train_file $root_dir/MaCoCuV1.en-sq.tsv.dedup \
-    --dev_file $root_dir/flores200.dev.en-sq.tsv.dedup \
+    --train_file $root_dir/data/MaCoCuV1.en-sq.tsv.dedup \
+    --dev_file $root_dir/data/flores200.dev.en-sq.tsv.dedup \
     --wandb \
     --model_name Helsinki-NLP/opus-mt-en-sq \
     &> $log_file 
