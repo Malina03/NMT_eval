ffab363b76d27ac08fdb70059657340fc1ba1a0b
Malina03 <malina.chichirau@gmail>
2023-07-01 14:18:07 +0200

    fixed dir

--- src/train.sh
+++ src/train.sh
@@ -20,12 +20,14 @@ module load PyTorch/1.12.1-foss-2022a-CUDA-11.7.0
 #load environment
 source /home1/s3412768/.envs/nmt2/bin/activate
 
-root_dir="/scratch/hb-macocu/NMT_eval/en-sq"
-log_file="/scratch/hb-macocu/NMT_eval/en-sq/logs/train.log"
+corpus=MaCoCuV1
+
+root_dir=/scratch/hb-macocu/NMT_eval/en-sq
+log_file=/scratch/hb-macocu/NMT_eval/en-sq/logs/fine_tune/train_$corpus.log
 
 python /home1/s3412768/NMT_eval/src/train.py \
     --root_dir $root_dir \
-    --train_file $root_dir/data/MaCoCuV1.en-sq.tsv.dedup \
+    --train_file $root_dir/data/$corpus.en-sq.tsv.dedup \
     --dev_file $root_dir/data/flores200.dev.en-sq.tsv.dedup \
     --wandb \
     --model_name Helsinki-NLP/opus-mt-en-sq \
