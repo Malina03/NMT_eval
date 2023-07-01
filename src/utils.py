c33c1d5b80f65e2493f1385937c46eaa8734705f
Malina03 <malina.chichirau@gmail>
2023-07-01 19:07:04 +0200

    changed default early stopping

--- src/utils.py
+++ src/utils.py
@@ -40,7 +40,7 @@ def get_args():
     parser.add_argument("-evaluation_steps", "--evaluation_steps", required=False, type=int, default=10000, help="Evaluation steps.")
     parser.add_argument("-save_total_limit", "--save_total_limit", required=False, type=int, default=1, help="Maximum number of checkpoints to save.")
     parser.add_argument("-save_steps", "--save_steps", required=False, type=int, default=10000, help="Save checkpoint every X updates steps.")
-    parser.add_argument("-early_stopping", "--early_stopping", required=False, type=int, default=3, help="Early stopping patience.")
+    parser.add_argument("-early_stopping", "--early_stopping", required=False, type=int, default=1, help="Early stopping patience.")
     parser.add_argument("-label_smoothing", "--label_smoothing", required=False, type=float, default=0.1, help="Label smoothing.")
     args = parser.parse_args()
     return args
