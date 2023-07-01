d4343d24497065a5443f6178ca0c6a2e0b37aa7d
Malina03 <malina.chichirau@gmail>
2023-07-01 17:08:31 +0200

    changed node type

--- src/train.sh
+++ src/train.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=12:59:59
 #SBATCH --job-name=en-sq
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --mem=50G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.chichirau@student.rug.nl
