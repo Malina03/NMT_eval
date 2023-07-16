b0c438581fc0c9e835414528b9676809e506b393
Malina03 <malina.chichirau@gmail>
2023-07-16 13:33:28 +0200

    memory

--- job_scripts/train_en-tr_v2.sh
+++ job_scripts/train_en-tr_v2.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=tr-v2
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
-#SBATCH --mem=20G
+#SBATCH --mem=50G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.chichirau@student.rug.nl
 
